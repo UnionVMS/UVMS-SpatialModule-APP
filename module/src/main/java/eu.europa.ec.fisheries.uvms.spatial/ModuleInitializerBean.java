d19306b4993722f13bcf870eefc74e599ab1a72c
emkopycmi <emkopycmi@mycompany.com>
2015-07-06 14:52:01 +0000

    SC#UNIONVMS-415 Creating application deployment descriptor for USM Component
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@2641 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- module/src/main/java/eu.europa.ec.fisheries.uvms.spatial/ModuleInitializerBean.java
+++ module/src/main/java/eu.europa.ec.fisheries.uvms.spatial/ModuleInitializerBean.java
@@ -25,56 +25,69 @@ import java.util.Properties;
 @Startup
 public class ModuleInitializerBean {
 
+    private static final String PROP_MODULE_NAME = "module.name";
+    private static final String PROP_USM_REST_SERVER = "usm.rest.server";
+    private static final String PROP_USM_DESCRIPTOR_FORCE_UPDATE = "usm.deplyment.descriptor.force-update";
 
-    public final static String PROP_MODULE_NAME = "module.name";
-    public final static String PROP_USM_REST_SERVER = "usm.rest.server";
-    public final static String PROP_USM_DESCRIPTOR_FORCE_UPDATE = "usm.deplyment.descriptor.force-update";
+    private static final String USM_REST_DESCRIPTOR_URI = "/usm-administration/rest/deployments/";
+    private static final String CONFIG_USM_DEPLOYMENT_DESCRIPTOR_XML = "config.usmDeploymentDescriptor.xml";
+    private static final String CONFIG_PROPERTIES = "config.properties";
+    private static final String TRUE = "true";
 
-    public final static String USM_REST_DESCRIPTOR_URI = "/usm-administration/rest/deployments/";
-
-    final static Logger LOG = LoggerFactory.getLogger(ModuleInitializerBean.class);
-
+    private static final Logger LOG = LoggerFactory.getLogger(ModuleInitializerBean.class);
 
     @PostConstruct
     private void startup() throws IOException, JAXBException {
-        //do something on application startup
+        // do something on application startup
         Properties moduleConfigs = new Properties();
-        moduleConfigs.load(getClass().getResourceAsStream("config.properties"));
+        moduleConfigs.load(getClass().getResourceAsStream(CONFIG_PROPERTIES));
 
         Client c = ClientBuilder.newClient();
         WebTarget target = c.target(moduleConfigs.getProperty(PROP_USM_REST_SERVER)).path(USM_REST_DESCRIPTOR_URI);
 
         Response response = target.path(moduleConfigs.getProperty(PROP_MODULE_NAME)).request(MediaType.APPLICATION_XML_TYPE).get();
 
-        if (response.getStatus() != HttpServletResponse.SC_OK) {//TODO This is just an assumption that USM restful service  returns 200
+        USMDeploymentDescriptor descriptor = createDescriptor();
+
+        // TODO This is just an assumption that USM restful service returns 200
+        if (response.getStatus() != HttpServletResponse.SC_OK) {
             LOG.info("USM doesn't recognize the current module. Deploying module deployment descriptor...");
-            JAXBContext context = JAXBContext.newInstance(USMDeploymentDescriptor.class);
-            Unmarshaller un = context.createUnmarshaller();
-
-            USMDeploymentDescriptor descriptor = (USMDeploymentDescriptor) un.unmarshal(getClass().getResourceAsStream("config.usmDeploymentDescriptor.xml"));
-
             response = target.request(MediaType.APPLICATION_XML_TYPE).post(Entity.xml(descriptor));
 
-            if (response.getStatus() == HttpServletResponse.SC_OK) {
-                LOG.info("Application deployment descriptor successfully deployed into USM.");
-            } else {
-                throw new RuntimeException("Unable to deploy application descriptor into USM. Response code: " + response.getStatus() + ". Response body: " + response.toString());
-            }
+            checkResult(response, "");
 
         } else {
             LOG.info("Module deployment descriptor has already been deployed at USM.");
 
-            if ("true".equalsIgnoreCase(moduleConfigs.getProperty(PROP_USM_DESCRIPTOR_FORCE_UPDATE))) {
+            if (TRUE.equalsIgnoreCase(moduleConfigs.getProperty(PROP_USM_DESCRIPTOR_FORCE_UPDATE))) {
                 LOG.info("Updating the existing module deployment descriptor into USM.");
 
-                //TODO PUT the descriptor to update it in USM @see https://webgate.ec.europa.eu/CITnet/confluence/display/UNIONVMS/Deploy+Application
-                //create a test which mocks the USM services (Jersey consumer and producer of those deployment descriptors
-                //maybe an integration test
+                // TODO PUT the descriptor to update it in USM @see https://webgate.ec.europa.eu/CITnet/confluence/display/UNIONVMS/Deploy+Application
+                response = target.request(MediaType.APPLICATION_XML_TYPE).put(Entity.xml(descriptor));
 
+                checkResult(response, "re");
+
+                // create a test which mocks the USM services (Jersey consumer and producer of those deployment descriptors
+                // maybe an integration test
             }
         }
 
         c.close();
+    }
+
+    private USMDeploymentDescriptor createDescriptor() throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(USMDeploymentDescriptor.class);
+        Unmarshaller un = context.createUnmarshaller();
+
+        return (USMDeploymentDescriptor) un.unmarshal(getClass().getResourceAsStream(CONFIG_USM_DEPLOYMENT_DESCRIPTOR_XML));
+    }
+
+    private void checkResult(Response response, String prefix) {
+        if (response.getStatus() == HttpServletResponse.SC_OK) {
+            LOG.info("Application " + prefix + "deployment descriptor successfully " + prefix + "deployed into USM.");
+        } else {
+            throw new RuntimeException("Unable to " + prefix + "deploy application descriptor into USM. Response code: " + response.getStatus() + ". Response body: " + response.toString());
+        }
     }
 
     @PreDestroy
