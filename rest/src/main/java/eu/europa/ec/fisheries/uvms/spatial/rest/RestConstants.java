e2761a60b25d8d6a04172465ea2e65e38a22864f
emrinalgr <emrinalgr@mycompany.com>
2015-07-21 18:16:12 +0000

    delete duplicate class
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@2973 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/RestConstants.java
+++
@@ -1,8 +0,0 @@
-package eu.europa.ec.fisheries.uvms.spatial.rest;
-
-public class RestConstants {
-
-    public static final String MODULE_REST = "/rest";
-    public static final String MODULE_NAME = "/spatial";
-
-}
