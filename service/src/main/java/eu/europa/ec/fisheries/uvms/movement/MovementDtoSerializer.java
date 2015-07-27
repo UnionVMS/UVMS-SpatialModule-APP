ffd59246e6b90b305f8442b57646dcbcdbd87015
emrinalgr <emrinalgr@mycompany.com>
2015-07-27 16:49:45 +0000

    MovementDto serializer
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3145 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/MovementDtoSerializer.java
@@ -0,0 +1,74 @@
+package eu.europa.ec.fisheries.uvms.movement;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import eu.europa.ec.fisheries.schema.movement.v1.MovementPoint;
+import eu.europa.ec.fisheries.uvms.movement.MovementDto;
+import org.apache.commons.lang3.StringUtils;
+import org.geojson.LngLatAlt;
+import org.geojson.Point;
+
+import java.io.IOException;
+
+public class MovementDtoSerializer extends JsonSerializer<MovementDto> {
+
+    @Override
+    public void serialize(MovementDto value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+
+        MovementPoint position = value.getPosition();
+
+        gen.writeStartObject();
+        gen.writeStringField("type", "Feature");
+        gen.writeFieldName("geometry");
+
+        if (position != null){
+            double latitude = position.getLatitude();
+            double longitude = position.getLongitude();
+            Point point = new Point();
+            LngLatAlt coordinates = new LngLatAlt();
+            coordinates.setLatitude(latitude);
+            coordinates.setLongitude(longitude);
+            point.setCoordinates(coordinates);
+            writePoint(gen, point);
+        }
+
+        writeProperties(gen, value);
+        gen.writeEndObject();
+    }
+
+    private void writeProperties(JsonGenerator gen, MovementDto value) throws IOException{
+        gen.writeFieldName("properties");
+        gen.writeStartObject();
+        gen.writeStringField("cid", value.getConnectId());
+        gen.writeStringField("st", value.getStatus());
+        gen.writeNumberField("cs", value.getCalculatedSpeed());
+        gen.writeNumberField("co", value.getCourse());
+        gen.writeNumberField("ms", value.getMeasuredSpeed());
+        gen.writeStringField("mt", value.getMessageType().value());
+
+        if (value.getPositionTime() != null){
+            String s = value.getPositionTime().toString();
+            if (StringUtils.isNotBlank(s)){
+                gen.writeStringField("pt", s);
+            }
+        }
+
+        gen.writeEndObject();
+    }
+
+    private void writePoint(JsonGenerator gen, Point p) throws IOException {
+        gen.writeStartObject();
+        gen.writeStringField("type", "Point");
+        gen.writeFieldName("coordinates");
+        writePointCoords(gen, p);
+        gen.writeEndObject();
+    }
+
+    private void writePointCoords(JsonGenerator gen, Point p) throws IOException {
+        gen.writeStartArray();
+        gen.writeNumber(p.getCoordinates().getLongitude());
+        gen.writeNumber(p.getCoordinates().getLatitude());
+        gen.writeEndArray();
+    }
+}
