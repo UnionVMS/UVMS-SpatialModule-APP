4f37effd7823096f87fe809f1c46cd8c2483e473
emrinalgr <emrinalgr@mycompany.com>
2015-07-27 16:48:40 +0000

    MovementMapper
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3143 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/movement/MovementBaseTypeMapper.java
@@ -0,0 +1,17 @@
+package eu.europa.ec.fisheries.uvms.movement;
+
+import eu.europa.ec.fisheries.schema.movement.v1.MovementBaseType;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * //TODO add test
+ */
+@Mapper
+public interface MovementBaseTypeMapper {
+
+    MovementBaseTypeMapper INSTANCE = Mappers.getMapper(MovementBaseTypeMapper.class);
+
+    MovementDto movementBaseTypeToMovementDto(MovementBaseType movementBaseType);
+}
+
