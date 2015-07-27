4628b7b2ea54f2a623d8b364d30afb74ec924205
Georgi Georgiev <georgi.georgiev@ordina.be>
2015-07-27 15:02:52 +0000

    * moving ModuleInitializerBean to service module * fixing the ModuleInitializerBean to use authentication and jwtoken authorization header
    (forgotten file)
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3137 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/dto/AuthenticationRequest.java
@@ -0,0 +1,29 @@
+package eu.europa.ec.fisheries.uvms.spatial.service.dto;
+
+import java.io.Serializable;
+
+/**
+ * Created by georgige on 7/24/2015.
+ */
+public class AuthenticationRequest implements Serializable{
+
+    private static final long serialVersionUID = -8088379939241999781L;
+
+    private String userName, password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
