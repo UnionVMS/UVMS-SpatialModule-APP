7048a581e9794c26bebe4d8efda7ac603174451b
emrinalgr <emrinalgr@mycompany.com>
2015-07-28 07:16:30 +0000

    fix build
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3161 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/SpatialService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/SpatialService.java
@@ -1,7 +1,7 @@
 package eu.europa.ec.fisheries.uvms.spatial.service;
 
+import eu.europa.ec.fisheries.uvms.spatial.dto.SpatialDto;
 import eu.europa.ec.fisheries.uvms.spatial.service.dao.entity.Eez;
-import eu.europa.ec.fisheries.uvms.spatial.service.dto.SpatialDto;
 import eu.europa.ec.fisheries.uvms.spatial.service.exception.SpatialServiceException;
 
 import javax.ejb.Local;
