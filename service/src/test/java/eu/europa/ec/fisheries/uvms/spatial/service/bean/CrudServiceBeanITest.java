0ad8cdad9cfeede434fb8d321fe74a01e90a917d
emrinalgr <emrinalgr@mycompany.com>
2015-08-04 18:14:05 +0000

    added todo
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3280 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/test/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/CrudServiceBeanITest.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/CrudServiceBeanITest.java
@@ -2,23 +2,17 @@ package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
 import eu.europa.ec.fisheries.uvms.spatial.entity.Country;
 import eu.europa.ec.fisheries.uvms.spatial.entity.ExclusiveEconomicZone;
-import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ejb.EJB;
-import java.io.File;
 
 import static junit.framework.TestCase.assertNotNull;
 
 @RunWith(Arquillian.class)
-public class CrudServiceBeanITest extends AbstractArquillianTest {
+public class CrudServiceBeanITest extends AbstractArquillianTest { // TODO make maven not read this test by renaming to CrudServiceBeanIT
 
     @EJB
     CrudService crudService;
@@ -32,6 +26,7 @@ public class CrudServiceBeanITest extends AbstractArquillianTest {
     public void shouldFindCountry() {
         // when
         Country country = (Country) crudService.find(Country.class, 1);
+
 
         //then
         assertNotNull(country);
