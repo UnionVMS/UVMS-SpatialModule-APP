7e2cef5a074d9695198a47853f710432ee370b18
emrinalgr <emrinalgr@mycompany.com>
2015-08-06 22:44:45 +0000

    friendly remark :-)
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3328 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/GeometryMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/GeometryMapper.java
@@ -8,6 +8,8 @@ import com.vividsolutions.jts.geom.Geometry;
 public class GeometryMapper {
     public String asString(Geometry geometry) {
         // TODO Fix it - please return GeoJSON
+        // Guys this is wrong JSON belongs in rest layer not service layer
+        // look as serializer deserializer example in MockResource please thanks guyz :-)
         return "test";
     }
 }
