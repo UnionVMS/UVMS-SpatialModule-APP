9ec2984ed44619410d60953c8ae5ee47a2b25afd
emkopycmi <emkopycmi@mycompany.com>
2015-08-07 14:43:43 +0000

    SC#UNIONVMS-497 BE: The system shall provide a service to expose the list of area types available (R0213)
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3344 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/ExclusiveEconomicZoneServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/ExclusiveEconomicZoneServiceBean.java
@@ -20,19 +20,19 @@ public class ExclusiveEconomicZoneServiceBean implements ExclusiveEconomicZoneSe
     @EJB
     private CrudDao crudDao;
 
+    @EJB
+    private EezMapper eezMapper;
+
     @Override
     public GetEezSpatialRS getExclusiveEconomicZoneById(long id) {
         EezEntity eez = (EezEntity) crudDao.find(EezEntity.class, id);
         return createResponse(eez);
     }
 
-    // TODO It is better to let the container (EJB) to manage the lifecycle of objects than manually create objects
-    // TODO It is very difficult to write test independent of the Mapper when you can not mock it. As far as we are using EJB Injection in other places we should try to Inject as many components to our class as possible.
-    // TODO http://mapstruct.org/documentation/#section-retr-mapper => 4.2 Using dependency injection
     private GetEezSpatialRS createResponse(EezEntity eez) {
         GetEezSpatialRS response = new GetEezSpatialRS();
         if (eez != null) {
-            response.setEez(EezMapper.INSTANCE.eezEntityToSchema(eez));
+            response.setEez(eezMapper.eezEntityToSchema(eez));
         }
         return response;
     }
--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/EezMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/EezMapper.java
@@ -1,24 +1,128 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.mapper;
 
-import eu.europa.ec.fisheries.schema.spatial.types.EezType;
+import com.google.common.collect.Lists;
+import com.vividsolutions.jts.geom.Geometry;
+import eu.europa.ec.fisheries.schema.spatial.types.AreaResultType;
+import eu.europa.ec.fisheries.schema.spatial.types.GeometryType;
+import eu.europa.ec.fisheries.schema.spatial.types.PropertyType;
+import eu.europa.ec.fisheries.schema.spatial.types.PropertyType.Entry;
 import eu.europa.ec.fisheries.uvms.spatial.entity.EezEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
-import org.mapstruct.factory.Mappers;
+
+import javax.ejb.Stateless;
+import java.util.ArrayList;
+
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 /**
  * Created by kopyczmi on 06-Aug-15.
  */
-@Mapper(uses = GeometryMapper.class)
-public interface EezMapper {
+@Stateless
+public class EezMapper {
 
-    EezMapper INSTANCE = Mappers.getMapper(EezMapper.class);
+    private static final String FEATURE = "Feature";
 
-    @Mappings({
-            @Mapping(source = "eez", target = "name"),
-            @Mapping(source = "geom", target = "geometry")
-    })
-    EezType eezEntityToSchema(EezEntity eezEntity);
+    public AreaResultType eezEntityToSchema(EezEntity eezEntity) {
+        AreaResultType areaResultType = new AreaResultType();
+
+        areaResultType.setType(FEATURE);
+        areaResultType.setGeometry(createGeometry(eezEntity.getGeom()));
+        areaResultType.setProperties(createProperties(eezEntity));
+
+        return areaResultType;
+    }
+
+    private PropertyType createProperties(EezEntity eezEntity) {
+        PropertyType propertyType = new PropertyType();
+        ArrayList<Entry> properties = Lists.newArrayList();
+        addLongitude(eezEntity, properties);
+        addLatitude(eezEntity, properties);
+        addCountry(eezEntity, properties);
+        addSovereign(eezEntity, properties);
+        addRemarks(eezEntity, properties);
+        addIso3Digit(eezEntity, properties);
+        addDateChang(eezEntity, properties);
+        addAreaM2(eezEntity, properties);
+        propertyType.setEntry(properties);
+        return propertyType;
+    }
+
+    private void addAreaM2(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (eezEntity.getAreaM2() != null) {
+            Entry entry = new Entry();
+            entry.setKey("areaM2");
+            entry.setValue(Double.toString(eezEntity.getAreaM2()));
+            properties.add(entry);
+        }
+    }
+
+    private void addDateChang(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (isNotEmpty(eezEntity.getDateChang())) {
+            Entry entry = new Entry();
+            entry.setKey("dateChang");
+            entry.setValue(eezEntity.getDateChang());
+            properties.add(entry);
+        }
+    }
+
+    private void addIso3Digit(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (isNotEmpty(eezEntity.getIso3Digit())) {
+            Entry entry = new Entry();
+            entry.setKey("iso3Digit");
+            entry.setValue(eezEntity.getIso3Digit());
+            properties.add(entry);
+        }
+    }
+
+    private void addRemarks(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (isNotEmpty(eezEntity.getRemarks())) {
+            Entry entry = new Entry();
+            entry.setKey("remarks");
+            entry.setValue(eezEntity.getRemarks());
+            properties.add(entry);
+        }
+    }
+
+    private void addSovereign(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (isNotEmpty(eezEntity.getSovereign())) {
+            Entry entry = new Entry();
+            entry.setKey("sovereign");
+            entry.setValue(eezEntity.getSovereign());
+            properties.add(entry);
+        }
+    }
+
+    private void addCountry(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (isNotEmpty(eezEntity.getCountry())) {
+            Entry entry = new Entry();
+            entry.setKey("country");
+            entry.setValue(eezEntity.getCountry());
+            properties.add(entry);
+        }
+    }
+
+    private void addLatitude(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (eezEntity.getLatitude() != null) {
+            Entry entry = new Entry();
+            entry.setKey("latitude");
+            entry.setValue(Double.toString(eezEntity.getLatitude()));
+            properties.add(entry);
+        }
+    }
+
+    private void addLongitude(EezEntity eezEntity, ArrayList<Entry> properties) {
+        if (eezEntity.getLongitude() != null) {
+            Entry entry = new Entry();
+            entry.setKey("longitude");
+            entry.setValue(Double.toString(eezEntity.getLongitude()));
+            properties.add(entry);
+        }
+    }
+
+    private GeometryType createGeometry(Geometry geom) {
+        GeometryType geometryType = new GeometryType();
+        geometryType.setType(geom.getGeometryType());
+        geometryType.setCoordinates(geom.toText());
+        return geometryType;
+    }
 
 }
