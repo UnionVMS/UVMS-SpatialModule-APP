9d60a26862db98e8c7c311c176261f911bf90979
emrinalgr <emrinalgr@mycompany.com>
2015-08-17 08:40:25 +0000

    Move dao to common lib
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3548 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/CommonGenericDao.java
@@ -0,0 +1,14 @@
+package eu.europa.ec.fisheries.uvms.spatial.dao;
+
+import eu.europa.ec.fisheries.uvms.service.JPACommonGenericDAO;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+@Stateless
+@LocalBean
+@TransactionAttribute(TransactionAttributeType.REQUIRED)
+public class CommonGenericDao extends JPACommonGenericDAO {
+}
