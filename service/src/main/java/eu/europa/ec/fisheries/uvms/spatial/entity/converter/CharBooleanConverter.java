87234b0b13eb2f2a06a847e1c4ffbbaa5c68357f
empadhyad <empadhyad@mycompany.com>
2015-08-19 15:37:13 +0000

    UNIONVMS-602: Adding constants and converter class
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3635 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/converter/CharBooleanConverter.java
@@ -0,0 +1,24 @@
+package eu.europa.ec.fisheries.uvms.spatial.entity.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class CharBooleanConverter implements AttributeConverter<Boolean, String> {
+
+	@Override
+	public String convertToDatabaseColumn(Boolean attribute) {
+		if (Boolean.TRUE.equals(attribute)) {
+            return "Y";
+        } else {
+            return "N";
+        }
+	}
+
+	@Override
+	public Boolean convertToEntityAttribute(String dbData) {
+		return "Y".equals(dbData);
+	}
+
+}
+
---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/util/SpatialConstants.java
@@ -0,0 +1,16 @@
+package eu.europa.ec.fisheries.uvms.spatial.util;
+
+/**
+ * All constants for Spatial modules
+ * @author padhyad 
+ *
+ */
+public class SpatialConstants {
+	
+	/** Find all */
+    public static final String FIND_ALL = "AreaType.findAll";
+    
+    /** Find System */
+    public static final String FIND_SYSTEM = "AreaType.findSystem";
+
+}
