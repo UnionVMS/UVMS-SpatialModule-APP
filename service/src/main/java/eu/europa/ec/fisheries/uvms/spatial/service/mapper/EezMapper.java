539b652fd70a97cd1af9424de08154f13ca5cb67
emkopycmi <emkopycmi@mycompany.com>
2015-08-20 07:14:19 +0000

    Fixed failing test after using new Entities
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@3652 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/EezMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/EezMapper.java
@@ -3,10 +3,15 @@ package eu.europa.ec.fisheries.uvms.spatial.service.mapper;
 import eu.europa.ec.fisheries.uvms.spatial.entity.EezEntity;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.EezType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "cdi", uses = GeometryMapper.class)
 public interface EezMapper {
 
+    @Mappings({
+            @Mapping(source = "geom", target = "geometry"),
+    })
     EezType eezEntityToEezType(EezEntity eezEntity);
 
 }
