df0373331c59129601d8b6f424f59db6c87fb87b
emrinalgr <emrinalgr@mycompany.com>
2015-08-29 10:00:37 +0000

    remove imports
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@4005 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/AreaTypeNamesService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/AreaTypeNamesService.java
@@ -1,8 +1,6 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeNamesSpatialRS;
-import eu.europa.ec.fisheries.uvms.spatial.model.schemas.ClosestAreaSpatialRQ;
-import eu.europa.ec.fisheries.uvms.spatial.model.schemas.ClosestAreaSpatialRS;
 
 import java.util.List;
 
