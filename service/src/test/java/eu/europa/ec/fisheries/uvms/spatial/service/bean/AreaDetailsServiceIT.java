db2982302f3ed49de16b593e5c7d4f7c953b84c4
empadhyad <empadhyad@mycompany.com>
2015-09-04 09:04:07 +0000

    Service GetAreaDetails arquillian test
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@4247 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/AreaDetailsServiceIT.java
@@ -0,0 +1,86 @@
+package eu.europa.ec.fisheries.uvms.spatial.service.bean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.ScopeType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaDetailsSpatialRequest;
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaDetailsSpatialResponse;
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.ErrorsType;
+
+@RunWith(Arquillian.class)
+public class AreaDetailsServiceIT {
+	
+    @Deployment
+    public static WebArchive createDeployment() {
+        WebArchive webArchive = ShrinkWrap.create(WebArchive.class, "spatial-service.war").addPackages(true, "eu.europa")
+                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+                .addAsResource("META-INF/orm.xml")
+                .addAsResource("config.properties")
+                .addAsResource("META-INF/jboss-deployment-structure.xml")
+                .addAsResource("nativeSql.properties")
+                .addAsResource("logback.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+        File[] files = Maven.resolver().loadPomFromFile("pom.xml").importDependencies(ScopeType.COMPILE, ScopeType.RUNTIME, ScopeType.TEST, ScopeType.PROVIDED).resolve().withTransitivity().as(File.class);
+        webArchive.addAsLibraries(files);
+
+        return webArchive;
+    }
+	
+	@EJB
+	private AreaDetailsService areaDetailsService;
+	
+	@Test
+	public void getEezAreaDetailsTest() {
+		AreaDetailsSpatialRequest request = new AreaDetailsSpatialRequest(new AreaTypeEntry("1", "eez"));		
+		AreaDetailsSpatialResponse response = areaDetailsService.getAreaDetails(request);
+		assertNull(response.getResponseMessage().getErrors());
+		assertNotNull(response.getResponseMessage().getSuccess());		
+		assertNotNull(response.getAreaDetails().getAreaProperty());
+		assertEquals(response.getAreaDetails().getAreaProperty().isEmpty(), false);
+	}
+	
+	@Test
+	public void getRfmoAreaDetailsTest() {
+		AreaDetailsSpatialRequest request = new AreaDetailsSpatialRequest(new AreaTypeEntry("1", "rfmo"));		
+		AreaDetailsSpatialResponse response = areaDetailsService.getAreaDetails(request);
+		assertNull(response.getResponseMessage().getErrors());
+		assertNotNull(response.getResponseMessage().getSuccess());		
+		assertNotNull(response.getAreaDetails().getAreaProperty());
+		assertEquals(response.getAreaDetails().getAreaProperty().isEmpty(), false);
+	}
+	
+	@Test
+	public void getAreaDetailsInvalidEntityTest() {
+		AreaDetailsSpatialRequest request = new AreaDetailsSpatialRequest(new AreaTypeEntry("1", "Invalid"));		
+		AreaDetailsSpatialResponse response = areaDetailsService.getAreaDetails(request);
+		ErrorsType errorType = response.getResponseMessage().getErrors();
+		assertEquals(errorType.getErrorMessage().get(0).getErrorCode(), "5009");
+	}
+	
+	@Test
+	public void GetAreaDetailsInvalidRowTest() {
+		AreaDetailsSpatialRequest request = new AreaDetailsSpatialRequest(new AreaTypeEntry("10000000", "eez"));		
+		AreaDetailsSpatialResponse response = areaDetailsService.getAreaDetails(request);
+		ErrorsType errorType = response.getResponseMessage().getErrors();
+		assertNotNull(errorType.getErrorMessage().get(0).getErrorCode());
+		assertEquals(errorType.getErrorMessage().get(0).getErrorCode(), "5010");
+	}
+	
+}
