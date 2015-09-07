e8101c6a5b7bbbefc462b29129e5d56e9ac685d1
emkopycmi <emkopycmi@mycompany.com>
2015-09-07 07:49:36 +0000

    SC#UNIONVMS-498 BE: The system shall provide geoprocessing service to identify the closest point of a certain type from a given position (R0214)
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@4263 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/repository/SpatialRepository.java
@@ -0,0 +1,19 @@
+package eu.europa.ec.fisheries.uvms.spatial.repository;
+
+import com.vividsolutions.jts.geom.Point;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.ClosestAreaDto;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.ClosestLocationDto;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.MeasurementUnit;
+
+import java.util.List;
+
+/**
+ * Created by Michal Kopyczok on 21-Aug-15.
+ */
+public interface SpatialRepository {
+    List<Integer> findAreasIdByLocation(Point point, String areaDbTable);
+
+    List<ClosestAreaDto> findClosestArea(Point point, MeasurementUnit unit, String areaDbTable);
+
+    List<ClosestLocationDto> findClosestlocation(Point point, MeasurementUnit unit, String areaDbTable);
+}
