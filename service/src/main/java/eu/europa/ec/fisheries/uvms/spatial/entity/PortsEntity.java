7d6b03d244ebff6cdfcbac2f2231e96eaea35f4e
empadhyad <empadhyad@mycompany.com>
2015-09-09 16:14:25 +0000

    added column name alias
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@4366 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/PortsEntity.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/PortsEntity.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.annotation.ColumnAliasName;
+
 @Entity
 @Table(name = "ports", schema = "spatial")
 public class PortsEntity implements Serializable {
@@ -18,6 +20,7 @@ public class PortsEntity implements Serializable {
 	@Id
 	@Column(name = "gid")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ColumnAliasName(aliasName="gid")
 	private long gid;
 
 	public PortsEntity() {
