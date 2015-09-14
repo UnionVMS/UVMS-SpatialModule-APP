af89cc6a3c7a81c9d48a92f3438eabf2e2eba554
emrinalgr <emrinalgr@mycompany.com>
2015-09-14 14:24:43 +0000

    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@4488 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/resources/AreaTypeResource.java
@@ -0,0 +1,94 @@
+package eu.europa.ec.fisheries.uvms.spatial.rest.resources;
+
+import eu.europa.ec.fisheries.uvms.spatial.rest.dto.ResponseCode;
+import eu.europa.ec.fisheries.uvms.spatial.rest.dto.ResponseDto;
+import eu.europa.ec.fisheries.uvms.spatial.rest.error.ErrorHandler;
+import eu.europa.ec.fisheries.uvms.spatial.rest.util.ValidationUtils;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.AreaByLocationService;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.AreaTypeNamesService;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.ClosestAreaService;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.AreaDto;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.ClosestAreaDto;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceErrors;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import static eu.europa.ec.fisheries.uvms.spatial.rest.util.ValidationUtils.validateInputParameters;
+import static org.apache.commons.collections.CollectionUtils.isEmpty;
+
+@Path("/")
+public class AreaTypeResource {
+
+    final static Logger LOG = LoggerFactory.getLogger(AreaTypeResource.class);
+
+    @EJB
+    private AreaTypeNamesService areaTypeService;
+
+    @EJB
+    private AreaByLocationService areaByLocationService;
+
+    @EJB
+    private ClosestAreaService closestAreaService;
+
+    public AreaTypeResource() {
+    }
+
+    @GET
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    @Path("/areatypes")
+    public ResponseDto getAreaTypes() {
+        try {
+            LOG.info("Getting user areas list");
+            List<String> areaTypes = areaTypeService.listAllAreaTypeNames();
+            return new ResponseDto(areaTypes, ResponseCode.OK);
+        } catch (Exception ex) {
+            LOG.error("[ Error when getting area types list. ] ", ex);
+            return ErrorHandler.getFault(ex);
+        }
+    }
+
+    @GET
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    @Path("/areasbylocation")
+    public ResponseDto getAreasByLocation(
+            @QueryParam(value = "lat") Double lat,
+            @QueryParam(value = "lon") Double lon,
+            @DefaultValue("4326") @QueryParam(value = "crs") int crs) {
+        try {
+            LOG.info("Getting areas by location");
+            validateInputParameters(lat, lon);
+            List<AreaDto> areasByLocation = areaByLocationService.getAreaTypesByLocation(lat, lon, crs);
+            return new ResponseDto(areasByLocation, ResponseCode.OK);
+        } catch (Exception ex) {
+            LOG.error("[ Error when getting areas by location. ] ", ex);
+            return ErrorHandler.getFault(ex);
+        }
+    }
+
+    @GET
+    @Produces(value = {MediaType.APPLICATION_JSON})
+    @Path("/closestareas")
+    public ResponseDto closestArea(
+            @QueryParam(value = "lat") Double lat,
+            @QueryParam(value = "lon") Double lon,
+            @DefaultValue("4326") @QueryParam(value = "crs") int crs,
+            @DefaultValue("Meter") @QueryParam(value = "unit") String unit,
+            @QueryParam(value = "type") List<String> areaTypes) {
+        try {
+            LOG.info("Getting closest areas");
+            validateInputParameters(lat, lon);
+            List<ClosestAreaDto> closestAreas = closestAreaService.getClosestAreas(lat, lon, crs, unit, areaTypes);
+            return new ResponseDto(closestAreas, ResponseCode.OK);
+        } catch (Exception ex) {
+            LOG.error("[ Error when getting closest areas. ] ", ex);
+            return ErrorHandler.getFault(ex);
+        }
+    }
+
+}
