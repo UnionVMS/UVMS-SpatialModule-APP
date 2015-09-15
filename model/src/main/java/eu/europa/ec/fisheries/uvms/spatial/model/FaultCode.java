c96847c0fc28a4bf5fe1677c51aaa44c5265f5a6
emrinalgr <emrinalgr@mycompany.com>
2015-09-15 14:32:26 +0000

    UNIONVMS-662: remove faultcodes
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@4514 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- model/src/main/java/eu/europa/ec/fisheries/uvms/spatial/model/FaultCode.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/spatial/model/FaultCode.java
@@ -2,19 +2,7 @@ package eu.europa.ec.fisheries.uvms.spatial.model;
 
 public enum FaultCode {
 
-    SPATIAL_DOMAIN(1800),
-    SPATIAL_DOMAIN_MODEL(1810),
-    SPATIAL_DOMAIN_MODEL_GROUP(1811),
-    SPATIAL_DOMAIN_MESSAGE(1820),
-    SPATIAL_DOMAIN_MAPPING_ERROR(1830),
-    SPATIAL_DOMAIN_MAPPING_SEARCH(1831),
-    SPATIAL_DOMAIN_DAO(1840),
-    SPATIAL_DOMAIN_DAO_NO_ENTITY_FOUND(1841),
-    SPATIAL_DOMAIN_DAO_GROUP(1842),
-    SPATIAL_MESSAGE(1700),
-    SPATIAL_DOMAIN_INPUT_ERROR(1801);
-
-    //VesselModelException (parameterDao)
+    SPATIAL_MESSAGE(1700);
 
     private final int code;
 
