40a40fc38801d5d5dcdaa8a20d22459da6a2d5cb
emkopycmi <emkopycmi@mycompany.com>
2015-10-28 09:16:32 +0000

    UNIONVMS-811 Creating MapConfigService that returns Mock Map Configuration
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@6064 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/dto/config/FlagState.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/dto/config/FlagState.java
@@ -1,97 +1,26 @@
-
 package eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.config;
 
-import java.util.HashMap;
-import java.util.Map;
-import javax.annotation.Generated;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.annotation.Generated;
+import java.util.HashMap;
+import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
-//@JsonPropertyOrder({
-//    "dnk",
-//    "swe"
-//})
 public class FlagState {
 
-//    @JsonProperty("dnk")
-//    private String dnk;
-//    @JsonProperty("swe")
-//    private String swe;
     @JsonIgnore
     private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
     /**
      * No args constructor for use in serialization
-     * 
      */
     public FlagState() {
     }
-
-//    /**
-//     *
-//     * @param dnk
-//     * @param swe
-//     */
-//    public FlagState(String dnk, String swe) {
-//        this.dnk = dnk;
-//        this.swe = swe;
-//    }
-
-//    /**
-//     *
-//     * @return
-//     *     The dnk
-//     */
-//    @JsonProperty("dnk")
-//    public String getDnk() {
-//        return dnk;
-//    }
-//
-//    /**
-//     *
-//     * @param dnk
-//     *     The dnk
-//     */
-//    @JsonProperty("dnk")
-//    public void setDnk(String dnk) {
-//        this.dnk = dnk;
-//    }
-//
-//    public FlagState withDnk(String dnk) {
-//        this.dnk = dnk;
-//        return this;
-//    }
-//
-//    /**
-//     *
-//     * @return
-//     *     The swe
-//     */
-//    @JsonProperty("swe")
-//    public String getSwe() {
-//        return swe;
-//    }
-//
-//    /**
-//     *
-//     * @param swe
-//     *     The swe
-//     */
-//    @JsonProperty("swe")
-//    public void setSwe(String swe) {
-//        this.swe = swe;
-//    }
-//
-//    public FlagState withSwe(String swe) {
-//        this.swe = swe;
-//        return this;
-//    }
 
     @JsonAnyGetter
     public Map<String, Object> getAdditionalProperties() {
