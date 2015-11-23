dab69ebc84bd7216292b17d7153e91a9c4af3a89
empadhyad <empadhyad@mycompany.com>
2015-11-23 16:41:01 +0000

    Retriving MAP projection and layers
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@6918 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/MapConfigService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/MapConfigService.java
@@ -3,10 +3,15 @@ package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.SpatialEnrichmentRQ;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.SpatialSaveMapConfigurationRS;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.config.MapConfigDto;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.config.ProjectionDto;
+
+import java.util.List;
 
 public interface MapConfigService {
 
     MapConfigDto getReportConfig(int reportId);
+
+    List<ProjectionDto> getAllProjections();
 
     SpatialSaveMapConfigurationRS saveMapConfiguration(SpatialEnrichmentRQ spatialEnrichmentRQ);
 }
