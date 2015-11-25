0e4eb58b1d33e7eb179fea0202863d78d62f3c87
emrinalgr <emrinalgr@mycompany.com>
2015-11-25 13:45:36 +0000

    enumerated string
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@7006 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/ReportConnectSpatialEntity.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/ReportConnectSpatialEntity.java
@@ -7,19 +7,7 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import java.io.Serializable;
 import java.util.Set;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "report_connect_spatial", schema = "spatial")
@@ -58,13 +46,15 @@ public class ReportConnectSpatialEntity implements Serializable {
 	private String mapExtent;
 	
 	@Column(name = "display_format", length = 255)
+    @Enumerated(EnumType.STRING)
 	private CoordinatesFormat displayFormatType;
 	
 	@Column(name = "measurement_units", length = 255)
 	private String measurementUnits;
 	
 	@Column(name = "scalebar_units", length = 255)
-	private ScaleBarUnits scaleBarType;
+    @Enumerated(EnumType.STRING)
+    private ScaleBarUnits scaleBarType;
 
 	@Column(columnDefinition = "text", name = "vector_styles")
 	private String vectorStyles;
