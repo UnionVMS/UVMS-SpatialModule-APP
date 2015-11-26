12d3224da8448d729ead6c91fc256d3d0479e16e
emrinalgr <emrinalgr@mycompany.com>
2015-11-26 14:24:54 +0000

    fixing dto for frontend
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@7036 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/dto/MapSettingsDto.java
@@ -0,0 +1,18 @@
+package eu.europa.ec.fisheries.uvms.spatial.rest.dto;
+
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.MapConfigurationType;
+import lombok.experimental.Delegate;
+
+public class MapSettingsDto {
+
+    @Delegate(excludes = Exclude.class)
+    private MapConfigurationType mapConfigurationType;
+
+    public MapSettingsDto(MapConfigurationType mapConfigurationType) {
+        this.mapConfigurationType = mapConfigurationType;
+    }
+
+    private interface Exclude {
+        String getReportId();
+    }
+}
