43c74a9524aabc54b476e376ba1ee2ba844f600d
Georgi Georgiev <georgi.georgiev@ordina.be>
2016-01-11 17:18:33 +0000

    UNIONVMS-935 List user defined areas
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@7639 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/dto/UserAreaTypeDto.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/dto/UserAreaTypeDto.java
@@ -1,13 +1,14 @@
 package eu.europa.ec.fisheries.uvms.spatial.rest.dto;
 
+import eu.europa.ec.fisheries.uvms.spatial.util.SpatialTypeEnum;
+
 import java.io.Serializable;
 
 public class UserAreaTypeDto extends AreaTypeDto implements Serializable {
 
-    private static final String USER_AREA = "USER_AREA";
 
     public UserAreaTypeDto() {
-        super.setAreaType(USER_AREA);
+        super.setAreaType(SpatialTypeEnum.USERAREA.getType());
     }
 
 }
