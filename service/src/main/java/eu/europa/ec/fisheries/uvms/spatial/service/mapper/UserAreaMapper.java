1de285729cffed21807afa86584171751e0ef1f0
emkopycmi <emkopycmi@mycompany.com>
2016-01-12 11:03:03 +0000

    Added copying missing description to the mapper
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@7646 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/UserAreaMapper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/mapper/UserAreaMapper.java
@@ -17,12 +17,13 @@ public abstract class UserAreaMapper {
     }
 
     @Mappings({
-            @Mapping(source = "geometry", target = "geom"),
-            @Mapping(target = "isShared", expression = "java(userAreaDto.isShared())"),
             @Mapping(target = "name", expression = "java(userAreaDto.getName())"),
             @Mapping(target = "type", expression = "java(userAreaDto.getSubType())"),
+            @Mapping(target = "areaDesc", expression = "java(userAreaDto.getDesc())"),
+            @Mapping(source = "geometry", target = "geom"),
             @Mapping(target = "startDate", expression = "java(eu.europa.ec.fisheries.uvms.common.DateUtils.stringToDate(userAreaDto.getStartDate()))"),
-            @Mapping(target = "endDate", expression = "java(eu.europa.ec.fisheries.uvms.common.DateUtils.stringToDate(userAreaDto.getEndDate()))")
+            @Mapping(target = "endDate", expression = "java(eu.europa.ec.fisheries.uvms.common.DateUtils.stringToDate(userAreaDto.getEndDate()))"),
+            @Mapping(target = "isShared", expression = "java(userAreaDto.isShared())"),
     })
     public abstract UserAreasEntity fromDtoToEntity(UserAreaGeomDto userAreaDto);
 
