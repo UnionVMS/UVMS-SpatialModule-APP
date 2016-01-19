ec628a8e7b52b99d27cd63077650946ae53b22f1
Georgi Georgiev <georgi.georgiev@ordina.be>
2016-01-19 11:06:41 +0000

    parametrizing the USM descriptor re-deployment flag
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Spatial/APP@7777 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SpatialInitializerBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SpatialInitializerBean.java
@@ -1,14 +1,22 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 import eu.europa.ec.fisheries.uvms.init.AbstractModuleInitializerBean;
+import lombok.extern.slf4j.Slf4j;
 
 @Singleton
+@Slf4j
 public class SpatialInitializerBean extends AbstractModuleInitializerBean {
+
+    public static final String PROP_FILE_NAME = "config.properties";
+    public static final String PROP_USM_DESCRIPTOR_FORCE_UPDATE = "usm.deployment.descriptor.force-update";
 
     @Override
     protected InputStream getDeploymentDescriptorRequest() {
@@ -17,6 +25,26 @@ public class SpatialInitializerBean extends AbstractModuleInitializerBean {
 
     @Override
     protected boolean mustRedeploy() {
-        return true;
+        boolean isMustRedploy = false;
+        try {
+            Properties moduleConfigs = retrieveModuleConfigs();
+            isMustRedploy = Boolean.valueOf(moduleConfigs.getProperty(PROP_USM_DESCRIPTOR_FORCE_UPDATE));
+            log.info("{} file contains a configuration {}, with the following value {}", PROP_FILE_NAME, PROP_USM_DESCRIPTOR_FORCE_UPDATE, isMustRedploy);
+        } catch (IOException e) {
+            log.info("No {} file with property {} was configured. The default behavior is to skip USM deployment if application has already been deployed.", PROP_FILE_NAME, PROP_USM_DESCRIPTOR_FORCE_UPDATE);
+            //in case we can't retrieve a configuration, the default behavior is skipping redeployment
+        }
+        return isMustRedploy;
+    }
+
+    private Properties retrieveModuleConfigs() throws IOException {
+        Properties prop = new Properties();
+        InputStream properties = getClass().getClassLoader().getResourceAsStream(PROP_FILE_NAME);
+        if (properties != null) {
+            prop.load(properties);
+            return prop;
+        } else {
+            throw new FileNotFoundException("Property file '" + PROP_FILE_NAME + "' not found in the classpath");
+        }
     }
 }
