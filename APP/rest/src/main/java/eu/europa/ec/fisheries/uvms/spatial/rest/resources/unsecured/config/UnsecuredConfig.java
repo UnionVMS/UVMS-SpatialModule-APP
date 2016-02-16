032d9cd23d1aaea2ceb4788ace93b12572d60d0b
EMRINALGR <EMRINALGR@mycompany.com>
2016-02-16 12:15:18 +0000

    UNIONVMS-1112
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9088 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/resources/unsecured/config/UnsecuredConfig.java
+++ APP/rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/resources/unsecured/config/UnsecuredConfig.java
@@ -2,7 +2,6 @@ package eu.europa.ec.fisheries.uvms.spatial.rest.resources.unsecured.config;
 
 import eu.europa.ec.fisheries.uvms.spatial.rest.constants.RestConstants;
 import eu.europa.ec.fisheries.uvms.spatial.rest.resources.unsecured.LegendResource;
-import eu.europa.ec.fisheries.uvms.spatial.rest.resources.unsecured.SegmentResource;
 import eu.europa.ec.fisheries.uvms.spatial.rest.resources.unsecured.PositionResource;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -15,7 +14,6 @@ public class UnsecuredConfig extends Application  {
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new HashSet<>();
-        resources.add(SegmentResource.class);
         resources.add(LegendResource.class);
         resources.add(PositionResource.class);
         return resources;
