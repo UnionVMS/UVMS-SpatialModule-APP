aed45c82ba18c85abfc355a3711ef0a33137b66d
emkopycmi <emkopycmi@mycompany.com>
2016-02-17 13:04:12 +0000

    UploadAreaService -- created common interface used in abstract
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9108 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/handler/AbstractSaverHandler.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/handler/AbstractSaverHandler.java
@@ -1,12 +1,31 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean.handler;
 
 import com.google.common.collect.Maps;
+import eu.europa.ec.fisheries.uvms.exception.ServiceException;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.AreaDisableService;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceErrors;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceException;
 import org.opengis.feature.Property;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-public class AbstractSaverHandler {
+public abstract class AbstractSaverHandler {
+
+    protected abstract void saveNewAreas(Map<String, List<Property>> features, Date enabledOn) throws ServiceException;
+
+    protected abstract AreaDisableService getAreaDisableService();
+
+    public void replaceAreas(Map<String, List<Property>> features) throws ServiceException {
+        getAreaDisableService().disableAllAreas();
+        try {
+            Date enabledOn = new Date();
+            saveNewAreas(features, enabledOn);
+        } catch (Exception e) {
+            throw new SpatialServiceException(SpatialServiceErrors.INVALID_UPLOAD_AREA_DATA, e);
+        }
+    }
 
     protected Map<String, Object> createAttributesMap(List<Property> properties) {
         Map<String, Object> resultMap = Maps.newHashMap();
