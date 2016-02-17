1a4816e514ed403423dc1ea539ba95b724f7f2ef
emkopycmi <emkopycmi@mycompany.com>
2016-02-17 11:59:14 +0000

    UploadAreaService -- added fields validation
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@9107 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/handler/RfmoSaverHandler.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/handler/RfmoSaverHandler.java
@@ -4,6 +4,8 @@ import com.vividsolutions.jts.geom.Geometry;
 import eu.europa.ec.fisheries.uvms.exception.ServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.RfmoService;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.RfmoDto;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceErrors;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.opengis.feature.Property;
 
@@ -11,7 +13,6 @@ import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.transaction.Transactional;
-import java.math.BigInteger;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -32,19 +33,23 @@ public class RfmoSaverHandler extends AbstractSaverHandler implements SaverHandl
     }
 
     private void saveNewAreas(Map<String, List<Property>> features) throws ServiceException {
-        Date enabledOn = new Date();
-        for (List<Property> properties : features.values()) {
-            Map<String, Object> values = createAttributesMap(properties);
+        try {
+            Date enabledOn = new Date();
+            for (List<Property> properties : features.values()) {
+                Map<String, Object> values = createAttributesMap(properties);
 
-            RfmoDto rfmoDto = new RfmoDto();
-            rfmoDto.setGeometry((Geometry) values.get("the_geom"));
-            rfmoDto.setCode((String) values.get("code"));
-            rfmoDto.setName((String) values.get("name"));
-            rfmoDto.setTuna((String) values.get("tuna"));
-            rfmoDto.setEnabled(true);
-            rfmoDto.setEnabledOn(enabledOn);
+                RfmoDto rfmoDto = new RfmoDto();
+                rfmoDto.setGeometry((Geometry) values.get("the_geom"));
+                rfmoDto.setCode((String) values.get("code"));
+                rfmoDto.setName((String) values.get("name"));
+                rfmoDto.setTuna((String) values.get("tuna"));
+                rfmoDto.setEnabled(true);
+                rfmoDto.setEnabledOn(enabledOn);
 
-            rfmoService.createRfmo(rfmoDto);
+                rfmoService.createRfmo(rfmoDto);
+            }
+        } catch (Exception e) {
+            throw new SpatialServiceException(SpatialServiceErrors.INVALID_UPLOAD_AREA_DATA, e);
         }
     }
 
