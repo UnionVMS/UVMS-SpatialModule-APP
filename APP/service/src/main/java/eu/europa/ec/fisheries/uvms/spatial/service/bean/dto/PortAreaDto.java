7989588bbcf4fdf855c9a997200bc382c16c8fa8
emkopycmi <emkopycmi@mycompany.com>
2016-02-18 15:44:07 +0000

    UploadAreaService -- added Port Areas Handler
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9140 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/dto/PortAreaDto.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/dto/PortAreaDto.java
@@ -1,22 +1,53 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean.dto;
 
-public class PortAreaDto extends GeoJsonDto {
+import com.vividsolutions.jts.geom.Geometry;
 
-    public Long getId() {
-        Object gid = properties.get(ID);
-        if (gid != null) {
-            try {
-                return Long.valueOf((String)gid);
-            } catch (NumberFormatException nfe ) {
-                //do nothing, later we return null anyway
-            }
-        }
+import java.util.Date;
 
-        return null;
+public class PortAreaDto {
+    private Geometry geometry;
+    private String code;
+    private String name;
+    private Boolean enabled;
+    private Date enabledOn;
+
+    public Geometry getGeometry() {
+        return geometry;
     }
 
-    public void setId(Long gid) {
-        properties.put(ID, String.valueOf(gid));
+    public void setGeometry(Geometry geometry) {
+        this.geometry = geometry;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Date getEnabledOn() {
+        return enabledOn;
+    }
+
+    public void setEnabledOn(Date enabledOn) {
+        this.enabledOn = enabledOn;
+    }
 }
---
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/dto/PortAreaGeomDto.java
@@ -0,0 +1,22 @@
+package eu.europa.ec.fisheries.uvms.spatial.service.bean.dto;
+
+public class PortAreaGeomDto extends GeoJsonDto {
+
+    public Long getId() {
+        Object gid = properties.get(ID);
+        if (gid != null) {
+            try {
+                return Long.valueOf((String)gid);
+            } catch (NumberFormatException nfe ) {
+                //do nothing, later we return null anyway
+            }
+        }
+
+        return null;
+    }
+
+    public void setId(Long gid) {
+        properties.put(ID, String.valueOf(gid));
+    }
+
+}
