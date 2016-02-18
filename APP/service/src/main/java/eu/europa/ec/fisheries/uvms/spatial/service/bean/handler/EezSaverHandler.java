5a1de9ed660b461ee777fbca2ecee0f7fce231f2
emkopycmi <emkopycmi@mycompany.com>
2016-02-18 15:56:42 +0000

    Refactored SaveHandler
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9141 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/handler/EezSaverHandler.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/handler/EezSaverHandler.java
@@ -27,30 +27,26 @@ public class EezSaverHandler extends AbstractSaverHandler implements SaverHandle
     private EezService eezService;
 
     @Override
-    protected void saveNewAreas(Map<String, List<Property>> features, Date enabledOn) throws ServiceException {
-        for (List<Property> properties : features.values()) {
-            Map<String, Object> values = createAttributesMap(properties);
+    protected void saveNewAreas(Map<String, Object> values, Date enabledOn) throws ServiceException {
+        EezDto eezDto = new EezDto();
+        eezDto.setName((String) values.get("name"));
+        eezDto.setCountry((String) values.get("country"));
+        eezDto.setSovereign((String) values.get("sovereign"));
+        eezDto.setRemarks((String) values.get("remarks"));
+        eezDto.setSovId((Long) values.get("sov_id"));
+        eezDto.setEezId((Long) values.get("eez_id"));
+        eezDto.setCode((String) values.get("code"));
+        eezDto.setMrgid(BigInteger.valueOf(((Double) values.get("mrgid")).longValue()));
+        eezDto.setDateChang((String) values.get("date_chang"));
+        eezDto.setAreaM2((Double) values.get("area_m2"));
+        eezDto.setLongitude((Double) values.get("longitude"));
+        eezDto.setLatitude((Double) values.get("latitude"));
+        eezDto.setMrgidEez((Long) values.get("mrgid_eez"));
+        eezDto.setGeometry((Geometry) values.get("the_geom"));
+        eezDto.setEnabledOn(enabledOn);
+        eezDto.setEnabled(true);
 
-            EezDto eezDto = new EezDto();
-            eezDto.setName((String) values.get("name"));
-            eezDto.setCountry((String) values.get("country"));
-            eezDto.setSovereign((String) values.get("sovereign"));
-            eezDto.setRemarks((String) values.get("remarks"));
-            eezDto.setSovId((Long) values.get("sov_id"));
-            eezDto.setEezId((Long) values.get("eez_id"));
-            eezDto.setCode((String) values.get("code"));
-            eezDto.setMrgid(BigInteger.valueOf(((Double) values.get("mrgid")).longValue()));
-            eezDto.setDateChang((String) values.get("date_chang"));
-            eezDto.setAreaM2((Double) values.get("area_m2"));
-            eezDto.setLongitude((Double) values.get("longitude"));
-            eezDto.setLatitude((Double) values.get("latitude"));
-            eezDto.setMrgidEez((Long) values.get("mrgid_eez"));
-            eezDto.setGeometry((Geometry) values.get("the_geom"));
-            eezDto.setEnabledOn(enabledOn);
-            eezDto.setEnabled(true);
-
-            eezService.createEzz(eezDto);
-        }
+        eezService.createEzz(eezDto);
     }
 
     @Override
