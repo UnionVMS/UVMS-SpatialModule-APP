0204973c8ee43432ca8b8aafae67438a0f1d8a44
emrinalgr <emrinalgr@mycompany.com>
2016-03-08 11:51:13 +0000

    [SC] 1203 translate buffer
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@9723 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/CalculationServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/CalculationServiceBean.java
@@ -1,20 +1,13 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.geom.Polygon;
-import com.vividsolutions.jts.io.ParseException;
+import eu.europa.ec.fisheries.uvms.exception.ServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.service.CalculateService;
 import lombok.extern.slf4j.Slf4j;
-import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.geometry.jts.WKTReader2;
 import org.geotools.geometry.jts.WKTWriter2;
-
 import javax.ejb.Local;
 import javax.ejb.Stateless;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Point2D;
 
 @Slf4j
 @Stateless
@@ -29,33 +22,16 @@ public class CalculationServiceBean implements CalculateService {
     }
 
     @Override
-    public String translate(final Double tx, final Double ty, final String wkt) {
+    public String translate(final Double tx, final Double ty, final String wkt) throws ServiceException {
 
         try {
 
             Geometry geometry = new WKTReader2().read(wkt);
-            com.vividsolutions.jts.geom.Coordinate[] sourceCoordinates = geometry.getCoordinates();
-            com.vividsolutions.jts.geom.Coordinate[] targetCoordinates = new com.vividsolutions.jts.geom.Coordinate[]{};
-            AffineTransform translate= AffineTransform.getTranslateInstance(tx, ty); // FIXME
+            Geometry translate = SpatialUtils.translate(tx, ty, geometry);
+            return new WKTWriter2().write(translate);
 
-            for (int i= 0; i < sourceCoordinates.length; i++){
-                com.vividsolutions.jts.geom.Coordinate sourceCoordinate = sourceCoordinates[i];
-                Point2D p = new Point2D.Double(sourceCoordinate.x,sourceCoordinate.y); // FIXME
-                Point2D transform = translate.transform(p, null);
-                targetCoordinates[i] = new com.vividsolutions.jts.geom.Coordinate(transform.getX(), transform.getY());
-            }
-
-            GeometryFactory geometryFactory = JTSFactoryFinder.getGeometryFactory();
-
-            LinearRing ring = geometryFactory.createLinearRing( targetCoordinates );
-            Polygon polygon = geometryFactory.createPolygon(ring, null );
-            return new WKTWriter2().write(polygon);
-
-        } catch (ParseException e) {
-            e.printStackTrace(); // FIXME
+        } catch (Exception ex) {
+            throw new ServiceException(ex.getMessage());
         }
-
-        return null;
-
     }
 }
