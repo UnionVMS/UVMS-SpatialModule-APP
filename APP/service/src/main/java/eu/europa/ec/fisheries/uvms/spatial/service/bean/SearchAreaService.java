96671cbb220693216ddf4a60fcd70369c5268d51
emrinalgr <emrinalgr@mycompany.com>
2016-03-08 12:41:54 +0000

    [SC] 1203 translate buffer
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9725 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaService.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.areaServices.FilterAreasDto;
 
 public interface SearchAreaService {
 
 	List<Map<String, String>> getAreasByFilter(String areaType, String filter);
 	
 	List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes);
-}
+
+ }
