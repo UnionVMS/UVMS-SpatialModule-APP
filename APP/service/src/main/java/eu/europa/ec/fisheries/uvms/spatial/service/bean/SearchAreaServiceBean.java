703ae88780d6fabdacd2773189a7b0a1bfeb27b7
emrinalgr <emrinalgr@mycompany.com>
2016-03-08 12:41:54 +0000

    [SC] 1203 translate buffer
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9725 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaServiceBean.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaServiceBean.java
@@ -11,34 +11,41 @@ import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.transaction.Transactional;
 
+import com.google.common.collect.ImmutableMap;
+import eu.europa.ec.fisheries.uvms.spatial.entity.AreaLocationTypesEntity;
+import eu.europa.ec.fisheries.uvms.spatial.entity.util.QueryNameConstants;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
 import eu.europa.ec.fisheries.uvms.spatial.service.SpatialRepository;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.areaServices.FilterAreasDto;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceErrors;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceException;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 @Stateless
 @Local(SearchAreaService.class)
-@Transactional
 @Slf4j
-public class SearchAreaServiceBean extends SpatialServiceBean implements SearchAreaService {
+public class SearchAreaServiceBean implements SearchAreaService {
 	
 	private static final String GID = "gid";
-	
-	private static final String AREA_TYPE = "areaType";
+    private static final String TYPE_NAME = "typeName";
+    private static final String AREA_TYPE = "areaType";
 	
     @EJB
     private SpatialRepository repository;
     
 	@Override
-	public List<Map<String, String>> getAreasByFilter(String areaType, String filter) {
+    @Transactional
+    public List<Map<String, String>> getAreasByFilter(String areaType, String filter) {
 		validateAreaType(areaType);
 		List<Map<String, String>> mapList = repository.findAreaByFilter(areaType, filter);
 		return updateAreaType(mapList, areaType);
 	}
-	
-	@Override
-	public List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes) {
+
+    @Override
+    @Transactional
+    public List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes) {
 		List<Map<String, String>> columnMapList = new ArrayList<Map<String, String>>();
 		for(AreaTypeEntry areaTypeEntry : areaTypes) {
 			validateArea(areaTypeEntry.getAreaType().value(), areaTypeEntry.getId());
@@ -75,7 +82,22 @@ public class SearchAreaServiceBean extends SpatialServiceBean implements SearchA
 		validateAreaType(areaType);
 		validateId(gid);
 	}
-	
+
+    protected void validateId(String id) {
+        if (!StringUtils.isNumeric(id)) {
+            throw new SpatialServiceException(SpatialServiceErrors.INVALID_ID_TYPE, id);
+        }
+    }
+
+    @SneakyThrows
+    private AreaLocationTypesEntity getAreaLocationType(String type) {
+        Map<String, String> parameters = ImmutableMap.<String, String>builder().put(TYPE_NAME, type.toUpperCase()).build();
+        List<AreaLocationTypesEntity> areasLocationTypes = repository.findEntityByNamedQuery(AreaLocationTypesEntity.class, QueryNameConstants.FIND_TYPE_BY_NAME, parameters, 1);
+        if (areasLocationTypes.isEmpty()) {
+            throw new SpatialServiceException(SpatialServiceErrors.INVALID_AREA_LOCATION_TYPE, areasLocationTypes);
+        }
+        return areasLocationTypes.get(0);
+    }
 	private void validateAreaType(String areaType) {
 		getAreaLocationType(areaType);
 	}
