73499ca9a2a0ff077e24dd424d9e48fb82e6b159
EMRINALGR <EMRINALGR@mycompany.com>
2016-03-15 19:04:30 +0000

    Merged 2 services together
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@9895 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SpatialService.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SpatialService.java
@@ -1,13 +1,17 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
 import eu.europa.ec.fisheries.uvms.exception.ServiceException;
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaByLocationSpatialRQ;
+import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaExtendedIdentifierType;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.ClosestLocationSpatialRQ;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.Location;
 
 import java.util.List;
 
-public interface ClosestLocationService {
+public interface SpatialService {
 
     List<Location> getClosestLocationByLocationType(ClosestLocationSpatialRQ request) throws ServiceException;
+
+    List<AreaExtendedIdentifierType> getAreaTypesByLocation(AreaByLocationSpatialRQ request);
 
 }
