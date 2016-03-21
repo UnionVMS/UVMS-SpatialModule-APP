4ebd1cc14f591213d3228d39dbb0cf13c4e0892f
empadhyad <empadhyad@mycompany.com>
2016-03-21 16:13:25 +0000

    Update: Area Group cql_all and cql_active
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@9984 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/ReportingServiceBean.java
@@ -0,0 +1,52 @@
+package eu.europa.ec.fisheries.uvms.spatial.service.bean;
+
+import eu.europa.ec.fisheries.uvms.exception.ServiceException;
+import eu.europa.ec.fisheries.uvms.message.MessageException;
+import eu.europa.ec.fisheries.uvms.reporting.model.exception.ReportingModelException;
+import eu.europa.ec.fisheries.uvms.reporting.model.mappper.ReportingModuleRequestMapper;
+import eu.europa.ec.fisheries.uvms.reporting.model.mappper.ReportingModuleResponseMapper;
+import eu.europa.ec.fisheries.uvms.reporting.model.schemas.ReportGetStartAndEndDateRS;
+import eu.europa.ec.fisheries.uvms.spatial.message.service.ReportingProducerBean;
+import eu.europa.ec.fisheries.uvms.spatial.message.service.SpatialModuleReceiverBean;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+
+/**
+ * Created by padhyad on 3/21/2016.
+ */
+@Stateless
+@Local(ReportingService.class)
+@Slf4j
+public class ReportingServiceBean implements ReportingService {
+
+    @EJB
+    private ReportingProducerBean producer;
+
+    @EJB
+    private SpatialModuleReceiverBean consumer;
+
+    @Override
+    public ReportGetStartAndEndDateRS getReportDates(Integer reportId, String userName, String scopeName, String timeStamp) throws ServiceException {
+        try {
+            String request = ReportingModuleRequestMapper.mapToSpatialSaveOrUpdateMapConfigurationRQ(timeStamp, reportId.longValue(), userName, scopeName);
+            String correlationId = producer.sendModuleMessage(request, consumer.getDestination());
+            Message message = consumer.getMessage(correlationId, TextMessage.class);
+            return ReportingModuleResponseMapper.mapToReportGetStartAndEndDateRS(getText(message), correlationId);
+        } catch (ReportingModelException | MessageException | JMSException e) {
+            throw new ServiceException("Error in communication with Reporting module", e);
+        }
+    }
+
+    private TextMessage getText(Message message) throws JMSException {
+        if (message instanceof TextMessage) {
+            return (TextMessage) message;
+        }
+        return null;
+    }
+}
