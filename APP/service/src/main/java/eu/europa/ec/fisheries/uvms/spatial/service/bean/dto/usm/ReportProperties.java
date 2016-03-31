e48981fbe592507369378b4bb04be4449d035ace
empadhyad <empadhyad@mycompany.com>
2016-03-31 14:42:59 +0000

    MapConfig service for map config without saving report
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@10762 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/dto/usm/ReportProperties.java
@@ -0,0 +1,37 @@
+package eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.usm;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by padhyad on 3/31/2016.
+ */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ReportProperties {
+
+    @JsonProperty("startDate")
+    private String startDate;
+
+    @JsonProperty("endDate")
+    private String endDate;
+
+    @JsonProperty("startDate")
+    public String getStartDate() {
+        return startDate;
+    }
+
+    @JsonProperty("startDate")
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    @JsonProperty("endDate")
+    public String getEndDate() {
+        return endDate;
+    }
+
+    @JsonProperty("endDate")
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
