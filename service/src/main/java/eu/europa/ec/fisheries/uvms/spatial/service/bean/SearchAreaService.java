c54a04525d0f889d610fbd916adbf8531795a9db
EMRINALGR <EMRINALGR@mycompany.com>
2016-04-06 14:49:09 +0000

    fixing more todo's
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@11154 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaService.java
@@ -2,10 +2,12 @@ package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
 import java.util.List;
 import java.util.Map;
+
+import eu.europa.ec.fisheries.uvms.exception.ServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
 
 public interface SearchAreaService {
 
-	List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes);
+	List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes) throws ServiceException;
 
  }
--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/SearchAreaServiceBean.java
@@ -10,12 +10,12 @@ import javax.ejb.Local;
 import javax.ejb.Stateless;
 import javax.transaction.Transactional;
 import com.google.common.collect.ImmutableMap;
+import eu.europa.ec.fisheries.uvms.exception.ServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.entity.AreaLocationTypesEntity;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
 import eu.europa.ec.fisheries.uvms.spatial.service.SpatialRepository;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceErrors;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceException;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
@@ -28,56 +28,49 @@ public class SearchAreaServiceBean implements SearchAreaService {
     private static final String TYPE_NAME = "typeName";
     private static final String AREA_TYPE = "areaType";
 
-    @EJB
-    private SpatialRepository repository;
+    private @EJB SpatialRepository repository;
 
     @Override
     @Transactional
-    public List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes) {
+    public List<Map<String, String>> getSelectedAreaColumns(List<AreaTypeEntry> areaTypes) throws ServiceException {
         List<Map<String, String>> columnMapList = new ArrayList<>();
+
         for(AreaTypeEntry areaTypeEntry : areaTypes) {
-            validateArea(areaTypeEntry.getAreaType().value(), areaTypeEntry.getId());
+
+            String gid = areaTypeEntry.getId();
+            String areaType = areaTypeEntry.getAreaType().value();
+
+            if (!StringUtils.isNumeric(gid)) {
+                throw new SpatialServiceException(SpatialServiceErrors.INVALID_ID_TYPE, gid);
+            }
+
+            Map<String, String> parameters = ImmutableMap.<String, String>builder().put(TYPE_NAME, areaType.toUpperCase()).build();
+
+            List<AreaLocationTypesEntity> areasLocationTypes =
+                    repository.findEntityByNamedQuery(AreaLocationTypesEntity.class, AreaLocationTypesEntity.FIND_TYPE_BY_NAME, parameters, 1); // TODO @Greg Dao
+
+            if (areasLocationTypes.isEmpty()) {
+                throw new SpatialServiceException(SpatialServiceErrors.INTERNAL_APPLICATION_ERROR, areasLocationTypes);
+            }
+
             columnMapList.add(getSelectedColumnMap(areaTypeEntry.getAreaType().value(), areaTypeEntry.getId()));
         }
+
         return columnMapList;
     }
 
     private Map<String, String> getSelectedColumnMap(String areaType, String gid) {
+
         String namedQuery = getNamedQueryByType(areaType);
-        Map<String, String> columnMap = getFirstMap(repository.findSelectedAreaColumns(namedQuery, Long.parseLong(gid)));
+        List<Map<String, String>> selectedAreaColumns = repository.findSelectedAreaColumns(namedQuery, Long.parseLong(gid));
+
+        if (selectedAreaColumns.isEmpty()) {
+            throw new SpatialServiceException(SpatialServiceErrors.ENTITY_NOT_FOUND);
+        }
+
+        Map<String, String> columnMap = selectedAreaColumns.get(0);
         columnMap.put(GID, gid);
         columnMap.put(AREA_TYPE, areaType.toUpperCase());
         return columnMap;
-    }
-
-    private Map<String, String> getFirstMap(List<Map<String, String>> listMap) {
-        if (listMap.isEmpty()) {
-            throw new SpatialServiceException(SpatialServiceErrors.ENTITY_NOT_FOUND);
-        }
-        return listMap.get(0);
-    }
-
-    private void validateArea(String areaType, String gid) {
-        validateAreaType(areaType);
-        validateId(gid);
-    }
-
-    protected void validateId(String id) {
-        if (!StringUtils.isNumeric(id)) {
-            throw new SpatialServiceException(SpatialServiceErrors.INVALID_ID_TYPE, id);
-        }
-    }
-
-    @SneakyThrows
-    private AreaLocationTypesEntity getAreaLocationType(String type) { // FIXME to be removed
-        Map<String, String> parameters = ImmutableMap.<String, String>builder().put(TYPE_NAME, type.toUpperCase()).build();
-        List<AreaLocationTypesEntity> areasLocationTypes = repository.findEntityByNamedQuery(AreaLocationTypesEntity.class, AreaLocationTypesEntity.FIND_TYPE_BY_NAME, parameters, 1);
-        if (areasLocationTypes.isEmpty()) {
-            throw new SpatialServiceException(SpatialServiceErrors.INTERNAL_APPLICATION_ERROR, areasLocationTypes);
-        }
-        return areasLocationTypes.get(0);
-    }
-    private void validateAreaType(String areaType) {
-        getAreaLocationType(areaType);
     }
 }
