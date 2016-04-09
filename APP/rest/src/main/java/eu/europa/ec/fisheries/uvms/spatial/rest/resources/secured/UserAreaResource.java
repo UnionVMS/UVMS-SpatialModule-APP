e684c68bb90d494468d2a0bdf7a03539325ca508
emrinalgr <emrinalgr@mycompany.com>
2016-04-09 18:04:07 +0000

    Bug Fix  geojson mapper
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@11204 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/resources/secured/UserAreaResource.java
+++ APP/rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/resources/secured/UserAreaResource.java
@@ -62,24 +62,23 @@ public class    UserAreaResource extends UnionVMSResource {
     public Response storeUserArea(@Context HttpServletRequest request,
                                   UserAreaGeoJsonDto userAreaGeoJsonDto,
                                   @HeaderParam(USMSpatial.SCOPE_NAME) String scopeName) throws ServiceException {
-     //   if (request.isUserInRole("MANAGE_USER_DEFINED_AREAS")) {
+        if (request.isUserInRole("MANAGE_USER_DEFINED_AREAS")) {
             String userName = request.getRemoteUser();
             log.info("{} is requesting storeUserArea(...)", userName);
-            //if (StringUtils.isNotBlank(userAreaGeoJsonDto.getDatasetName()) && !request.isUserInRole("CREATE_USER_AREA_DATASET")) {
-            //    return createErrorResponse("user_area_dataset_creation_not_allowed");
-            //}
+            if (StringUtils.isNotBlank(userAreaGeoJsonDto.getDatasetName()) && !request.isUserInRole("CREATE_USER_AREA_DATASET")) {
+                return createErrorResponse("user_area_dataset_creation_not_allowed");
+            }
 
-            //if (userAreaGeoJsonDto.getScopeSelection() != null && !userAreaGeoJsonDto.getScopeSelection().isEmpty() && !request.isUserInRole("SHARE_USER_DEFINED_AREAS")) {
-            //    return createErrorResponse("user_area_sharing_not_allowed");
-            //}
+            if (userAreaGeoJsonDto.getScopeSelection() != null && !userAreaGeoJsonDto.getScopeSelection().isEmpty() && !request.isUserInRole("SHARE_USER_DEFINED_AREAS")) {
+                return createErrorResponse("user_area_sharing_not_allowed");
+            }
 
             long gid = userAreaService.storeUserArea(userAreaGeoJsonDto, userName);
             return createSuccessResponse(gid);
-    //    } else {
-    //        return createErrorResponse("user_areas_management_not_allowed");
-    //    }
+        } else {
+            return createErrorResponse("user_areas_management_not_allowed");
+        }
     }
-
 
     @PUT
     @Path("userarea")
