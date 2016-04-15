3740eb6083743a896c34882b8c0eb520545486ee
emrinalgr <emrinalgr@mycompany.com>
2016-04-15 13:04:52 +0000

    base area
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@11382 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/StatRectEntity.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/StatRectEntity.java
@@ -1,36 +1,15 @@
 package eu.europa.ec.fisheries.uvms.spatial.entity;
 
-import java.io.Serializable;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import eu.europa.ec.fisheries.uvms.spatial.service.bean.annotation.ColumnAliasName;
 import org.hibernate.annotations.Where;
 
 @Entity
 @Where(clause = "enabled = 'Y'")
 @Table(name = "stat_rect", schema = "spatial")
-public class StatRectEntity implements Serializable {
-
-	@Id
-	@Column(name = "gid")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@ColumnAliasName(aliasName="gid")
-	private long gid;
+public class StatRectEntity extends BaseAreaEntity {
 
 	public StatRectEntity() {
-	}
-
-	public long getGid() {
-		return this.gid;
-	}
-
-	public void setGid(long gid) {
-		this.gid = gid;
 	}
 
 }
