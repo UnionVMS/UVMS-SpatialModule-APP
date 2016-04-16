6abaf1d6b3bce0b6392d29476b04c5927fdbd7bd
emrinalgr <emrinalgr@mycompany.com>
2016-04-16 11:14:01 +0000

    remove unused, merge
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@11488 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/RfmoService.java
+++
@@ -1,8 +0,0 @@
-package eu.europa.ec.fisheries.uvms.spatial.service.bean;
-
-import eu.europa.ec.fisheries.uvms.exception.ServiceException;
-import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.areaServices.RfmoDto;
-
-public interface RfmoService {
-    long createRfmo(RfmoDto rfmoDto) throws ServiceException;
-}
