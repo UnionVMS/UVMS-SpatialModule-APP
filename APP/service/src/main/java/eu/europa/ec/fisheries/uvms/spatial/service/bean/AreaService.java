c811dcfadf9b037db2810f66f4475cc593ed8070
emrinalgr <emrinalgr@mycompany.com>
2016-04-16 10:56:40 +0000

    comply with ejb contract
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@11486 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/AreaService.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/AreaService.java
@@ -3,7 +3,9 @@ package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 import eu.europa.ec.fisheries.uvms.exception.ServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaDetails;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
+import org.opengis.feature.Property;
 
+import java.util.List;
 import java.util.Map;
 
 public interface AreaService {
@@ -11,5 +13,13 @@ public interface AreaService {
     AreaDetails getAreaDetailsById(AreaTypeEntry areaTypeEntry) throws ServiceException;
 
     public Map<String, String> getAllCountriesDesc();
+
+    void replaceEezArea(Map<String, List<Property>> features);
+
+    void replaceRfmo(Map<String, List<Property>> features);
+
+    void replacePort(Map<String, List<Property>> features);
+
+    void replacePortArea(Map<String, List<Property>> features);
 
 }
