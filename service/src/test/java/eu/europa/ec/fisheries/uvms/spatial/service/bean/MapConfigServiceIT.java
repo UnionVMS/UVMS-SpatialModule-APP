a33fd225f6eefb6cf4a613e6f4ca69ac31c6d826
emrinalgr <emrinalgr@mycompany.com>
2016-04-23 15:39:18 +0000

    Move arquillian related to comon-test
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@11647 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/test/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/MapConfigServiceIT.java
+++ service/src/test/java/eu/europa/ec/fisheries/uvms/spatial/service/bean/MapConfigServiceIT.java
@@ -1,5 +1,6 @@
 package eu.europa.ec.fisheries.uvms.spatial.service.bean;
 
+import eu.europa.ec.fisheries.uvms.AbstractArquillianTest;
 import eu.europa.ec.fisheries.uvms.spatial.service.MapConfigService;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.config.MapConfigDto;
 import eu.europa.ec.fisheries.uvms.spatial.service.bean.dto.config.ServiceLayersDto;
@@ -23,7 +24,7 @@ import static org.junit.Assert.assertNull;
  * Created by padhyad on 11/20/2015.
  */
 @RunWith(Arquillian.class)
-public class MapConfigServiceIT extends AbstractArquillianIT {
+public class MapConfigServiceIT extends AbstractArquillianTest {
 
     @EJB
     private MapConfigService mapConfigService;
