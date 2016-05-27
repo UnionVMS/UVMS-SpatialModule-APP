23a94076a01d2ff7e077f133d20e3f0988930935
emrinalgr <emrinalgr@mycompany.com>
2016-05-27 14:28:21 +0000

    mapping oracle queries
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@12589 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/DatabaseDialect.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/DatabaseDialect.java
@@ -1,10 +1,10 @@
 package eu.europa.ec.fisheries.uvms.spatial.dao.util;
 
-public interface SpatialFunction {
+public interface DatabaseDialect {
 
     String stIntersects(Double latitude, Double longitude);
 
-    String closestAreaToPoint(String typeName, String tableName, Double latitude, Double longitude);
+    String closestAreaToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit);
 
     String closestPointToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit);
 
