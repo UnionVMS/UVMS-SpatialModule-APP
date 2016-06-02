27d2cd34f5dea062d012e44d8e2a4f2814b55f03
emrinalgr <emrinalgr@mycompany.com>
2016-06-02 09:14:25 +0000

    1462
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@12630 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/AreaService.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/AreaService.java
@@ -3,9 +3,11 @@ package eu.europa.ec.fisheries.uvms.spatial.service;
 import eu.europa.ec.fisheries.uvms.exception.ServiceException;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaDetails;
 import eu.europa.ec.fisheries.uvms.spatial.model.schemas.AreaTypeEntry;
-
+import eu.europa.ec.fisheries.uvms.spatial.model.upload.UploadMetadata;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import org.opengis.referencing.FactoryException;
 
 public interface AreaService {
 
@@ -13,8 +15,11 @@ public interface AreaService {
 
     void uploadArea(byte[] data, String areaType, int crsCode) throws ServiceException;
 
+    UploadMetadata metadata(byte[] data, String areaType) throws ServiceException;
+
     AreaDetails getAreaDetailsById(AreaTypeEntry areaTypeEntry) throws ServiceException;
 
     public Map<String, String> getAllCountriesDesc() throws ServiceException;
 
+    void upload(UploadMetadata metadata, String type, String code) throws ServiceException, FactoryException, IOException;
 }
