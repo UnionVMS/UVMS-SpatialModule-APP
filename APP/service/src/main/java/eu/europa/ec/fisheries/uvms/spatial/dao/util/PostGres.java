5d675aa749eff97c39c20bc9d141b1a6d99f3860
emdubandr <emdubandr@mycompany.com>
2016-06-17 12:24:48 +0000

    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@12896 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/PostGres.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/PostGres.java
@@ -4,7 +4,9 @@ public class PostGres extends AbstractGisFunction {
 
     @Override
     public String stIntersects(Double latitude, Double longitude) {
-        return "ST_Intersects(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326)) ";
+    	StringBuffer sb = new StringBuffer();    	    	
+        sb.append("ST_Intersects(geom, ST_GeomFromText(CAST ('POINT(").append(longitude).append(" ").append(latitude).append(")' AS TEXT), 4326)) ");
+        return sb.toString();
     }
 
     
@@ -20,28 +22,32 @@ public class PostGres extends AbstractGisFunction {
     
     @Override
     public String makeGeomValid(String tableName) {
-        return "update spatial."+ tableName + " set geom = st_makevalid(geom) where enabled = 'Y'";
+    	StringBuffer sb = new StringBuffer();    	
+    	sb.append("update spatial.").append(tableName).append(" set geom = st_makevalid(geom) where enabled = 'Y'");
+    	return sb.toString();
     }
     
     @Override
     public String closestAreaToPoint(int index,String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
-
-        return "(SELECT '" + typeName + "' AS type, gid, code, name," +
-                " ST_ClosestPoint(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326))" +
-                " AS closest, "+
-                " ST_Distance(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326),true) as dist "+                
-                " FROM spatial." + tableName +
-                " WHERE NOT ST_IsEmpty(geom) AND enabled = 'Y' " + "ORDER BY dist,gid " +
-                " LIMIT " + limit + ")";
+    	StringBuffer sb = new StringBuffer();
+    	sb.append("(SELECT '").append(typeName).append("' AS type, gid, code, name,");
+    	sb.append(" ST_ClosestPoint(geom, ST_GeomFromText(CAST ('POINT(").append(longitude).append(" ").append(latitude).append(")' AS TEXT), 4326))" );
+    	sb.append(" AS closest, ");
+    	sb.append(" ST_Distance(geom, ST_GeomFromText(CAST ('POINT(").append(longitude).append(" ").append(latitude).append(")' AS TEXT), 4326),true) as dist ");                
+    	sb.append(" FROM spatial.").append(tableName);
+    	sb.append(" WHERE NOT ST_IsEmpty(geom) AND enabled = 'Y' ORDER BY dist,gid ");
+    	sb.append(" LIMIT ").append(limit).append(")");
+    	return sb.toString();
     }
 
     @Override
     public String closestPointToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
-
-        return "(SELECT '" + typeName + "' as type, gid, code, name, geom, "
-                + "ST_Distance(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326),true) " +
-                "AS distance FROM spatial." + tableName + " WHERE enabled = 'Y'" +
-                " ORDER BY distance,gid ASC LIMIT " + limit + " )";
+    	StringBuffer sb = new StringBuffer();
+        sb.append("(SELECT '").append(typeName).append("' as type, gid, code, name, geom, ");
+        sb.append("ST_Distance(geom, ST_GeomFromText(CAST ('POINT(").append(longitude).append(" ").append(latitude).append(")' AS TEXT), 4326),true) ");
+        sb.append("AS distance FROM spatial.").append(tableName).append(" WHERE enabled = 'Y'");
+        sb.append(" ORDER BY distance,gid ASC LIMIT ").append(limit).append(" )");
+        return sb.toString();
     }
 
 }
