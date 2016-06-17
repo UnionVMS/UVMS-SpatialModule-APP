719af2e03d81558cd761bda986661e09913d19ec
Georgi Georgiev <georgi.georgiev@ordina.be>
2016-06-17 10:02:36 +0000

    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@12886 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/PostGres.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/PostGres.java
@@ -7,29 +7,36 @@ public class PostGres extends AbstractGisFunction {
         return "ST_Intersects(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326)) ";
     }
 
+    
     @Override
-    public String closestAreaToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
+    public String closestAreaToPointPrefix(){
+    	return "";    	
+    }
+    
+    @Override
+    public  String closestAreaToPointSuffix(){
+    	return "";
+    }
+    
+    @Override
+    public String closestAreaToPoint(int index,String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
 
         return "(SELECT '" + typeName + "' AS type, gid, code, name," +
                 " ST_ClosestPoint(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326))" +
-                " AS closest FROM spatial." + tableName +
-                " WHERE NOT ST_IsEmpty(geom) AND enabled = 'Y' " + "ORDER BY " +
-                " ST_Distance(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326)) "
-                + " LIMIT " + limit + ")";
+                " AS closest, "+
+                " ST_Distance(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326),true) as dist "+                
+                " FROM spatial." + tableName +
+                " WHERE NOT ST_IsEmpty(geom) AND enabled = 'Y' " + "ORDER BY dist,gid " +
+                " LIMIT " + limit + ")";
     }
 
     @Override
     public String closestPointToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
 
         return "(SELECT '" + typeName + "' as type, gid, code, name, geom, "
-                + "ST_Distance(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326)) " +
+                + "ST_Distance(geom, ST_GeomFromText(CAST ('POINT(" + longitude + " " + latitude + ")' AS TEXT), 4326),true) " +
                 "AS distance FROM spatial." + tableName + " WHERE enabled = 'Y'" +
-                " ORDER BY distance ASC LIMIT " + limit + " )";
-    }
-
-    @Override
-    public String makeGeomValid(String tableName) {
-        return "update spatial."+ tableName + " set geom = st_makevalid(geom) where enabled = 'Y'";
+                " ORDER BY distance,gid ASC LIMIT " + limit + " )";
     }
 
 }
