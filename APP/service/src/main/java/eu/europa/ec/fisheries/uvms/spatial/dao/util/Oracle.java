7dde79a7fb21b3ebd9d8ee7fa626065474ebc085
Georgi Georgiev <georgi.georgiev@ordina.be>
2016-06-17 10:00:52 +0000

    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@12884 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/Oracle.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/dao/util/Oracle.java
@@ -1,5 +1,8 @@
 package eu.europa.ec.fisheries.uvms.spatial.dao.util;
 
+import java.util.Iterator;
+
+
 public class Oracle extends AbstractGisFunction {
 
     @Override
@@ -7,32 +10,39 @@ public class Oracle extends AbstractGisFunction {
         return "SDO_RELATE(GEOM, SDO_GEOMETRY('POINT(" + longitude + " " + latitude + ")', 8307), 'mask=contains') = 'TRUE' AND enabled = 'Y'";
     }
 
+    
     @Override
-    public String closestAreaToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
+    public String closestAreaToPointPrefix(){
+    	return "select * from  (";    	
+    }
+    
+    @Override
+    public  String closestAreaToPointSuffix(){
+    	return ")  ORDER BY indexRS,dist,gid ASC ";
+    }
 
-     return "(SELECT '" + typeName + "' AS type, gid, code, name," + " SDO_NN_DISTANCE(1) AS dist, " +
+    
+    	
+    @Override
+    public String closestAreaToPoint(int index,String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
+
+     return "(select rownum, a.* from (SELECT "+index+" as indexRS, '" + typeName + "' AS type, gid, code, name, SDO_NN_DISTANCE(1) AS dist, " +
              "geom AS closest FROM spatial." + tableName + " WHERE enabled = 'Y' " +
-             "AND SDO_NN(geom, SDO_GEOMETRY('POINT(" + longitude + " " + latitude + ")', 8307), " +
-             "'sdo_num_res=" + limit + "', 1) = 'TRUE' ORDER BY dist ASC)";
+             "AND SDO_NN(geom, SDO_GEOMETRY('POINT(" + longitude + " " + latitude + ")', 8307), 1) = 'TRUE' "
+             		+ " ORDER BY dist,gid ASC) a where rownum <= "+limit+") ";
+     //
     }
 
     @Override
     public String closestPointToPoint(String typeName, String tableName, Double latitude, Double longitude, Integer limit) {
 
-        return "(SELECT '" + typeName + "' as type, gid, code, name, geom, "
+        return "select ROWNUM,s.* from (SELECT '" + typeName + "' as type, gid, code,name, geom, "
                 + stDistance(latitude, longitude) + " AS distance " +
                 "FROM spatial." + tableName + " WHERE enabled = 'Y' " +
-                "ORDER BY distance ASC OFFSET 0 ROWS FETCH NEXT " + limit + " ROWS ONLY)";
-    }
-
-    @Override
-    public String makeGeomValid(String tableName) {
-        return "update spatial." + tableName + " set geom = SDO_UTIL.RECTIFY_GEOMETRY(geom, 0.005) where enabled = 'Y'";
+                "ORDER BY distance,gid ASC) s where rownum<= "+ limit ;
     }
 
     private String stDistance(Double latitude, Double longitude) {
         return "SDO_GEOM.SDO_DISTANCE(geom, SDO_GEOMETRY('POINT(" + longitude + " " + latitude + ")', 8307), 0.05, 'unit=M')";
     }
-
-
 }
