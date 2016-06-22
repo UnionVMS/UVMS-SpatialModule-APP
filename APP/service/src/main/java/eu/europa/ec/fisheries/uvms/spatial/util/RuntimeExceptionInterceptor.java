0d423f557841b744f61bca4671660d8b8ed7f3f1
empadhyad <empadhyad@mycompany.com>
2016-06-22 10:13:18 +0000

    Fix for user area USM dataset transaction
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@12947 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/util/RuntimeExceptionInterceptor.java
@@ -0,0 +1,32 @@
+package eu.europa.ec.fisheries.uvms.spatial.util;
+
+import eu.europa.ec.fisheries.uvms.exception.ServiceException;
+import eu.europa.ec.fisheries.uvms.rest.constants.ErrorCodes;
+import eu.europa.ec.fisheries.uvms.rest.resource.UnionVMSResource;
+import eu.europa.ec.fisheries.uvms.spatial.service.bean.exception.SpatialServiceException;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+/**
+ * Created by padhyad on 6/22/2016.
+ */
+@Interceptor
+@Slf4j
+public class RuntimeExceptionInterceptor extends UnionVMSResource {
+
+    @AroundInvoke
+    public Object createResponse(final InvocationContext ic) {
+        log.info("ExceptionInterceptor received");
+        try {
+            return ic.proceed();
+        } catch (Exception e) {
+            if (e.getCause() instanceof ServiceException) {
+                throw new RuntimeException(((ServiceException)e.getCause()).getMessage());
+            }
+            throw new RuntimeException(e);
+        }
+    }
+}
