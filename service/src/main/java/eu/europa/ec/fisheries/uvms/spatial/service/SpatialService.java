2418307cb5d6906cdedd58bb8312db72c3121c55
empadhyad <empadhyad@mycompany.com>
2016-07-04 10:02:03 +0000

    Added Java Doc
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial/APP@13252 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/SpatialService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/SpatialService.java
@@ -18,8 +18,27 @@ public interface SpatialService {
 
     FilterAreasSpatialRS computeAreaFilter(FilterAreasSpatialRQ filterAreasSpatialRQ) throws ServiceException;
 
+    /**
+     * This API search for all the AREAS that has a matching <code>NAME</> or <code>CODE</> by Area Type and Filter in the input
+     *
+     * @param areaType Area Type to search
+     * @param filter Filter code
+     * @return Collection of Gid, code, area Type, extent of the area and Name
+     * @throws ServiceException Exception
+     */
     List<GenericSystemAreaDto> searchAreasByNameOrCode(String areaType, String filter) throws ServiceException;
 
+    /**
+     * <p>This API search for all the unique <code>AREA CODES<code/> and corresponding <code>AREA NAMES<code/> by Area Type and Filter text received.
+     * It groups the Area Code and the mapped Areas Names and combines with the extent of the areas.
+     * <p/>
+     * @param areaType Area Type to search
+     * @param filter Filter code
+     * @return Collection of area Names, combined extend and search code
+     * @throws ServiceException Exception
+     *
+     * @see SpatialService#searchAreasByNameOrCode(String, String)
+     */
     List<SystemAreaNamesDto> searchAreasByCode(String areaType, String filter) throws ServiceException;
 
     LocationDetails getLocationDetails(LocationTypeEntry locationTypeEntry) throws ServiceException;
