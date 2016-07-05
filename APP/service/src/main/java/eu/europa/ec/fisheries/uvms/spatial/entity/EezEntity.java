0030aae2ebf712647dbce3fb12b87913f0baaffe
empadhyad <empadhyad@mycompany.com>
2016-07-05 09:11:12 +0000

    Fix for not retrieving disabled areas in area by code service
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/trunk/Modules/Spatial@13272 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/EezEntity.java
+++ APP/service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/entity/EezEntity.java
@@ -21,7 +21,7 @@ import java.util.Map;
         @NamedQuery(name = EezEntity.DISABLE_EEZ_AREAS, query = "UPDATE EezEntity SET enabled = 'N'"),
         @NamedQuery(name = EezEntity.LIST_EMPTY_GEOMETRIES, query = "FROM EezEntity WHERE isEmpty(geom) = true AND enabled = 'Y'"),
         @NamedQuery(name = EezEntity.SEARCH_EEZ, query = "FROM EezEntity where upper(name) like :name OR upper(code) like :code AND enabled='Y' GROUP BY gid"),
-        @NamedQuery(name = EezEntity.SEARCH_EEZ_NAMES_BY_CODE, query = "From EezEntity where code in (SELECT distinct(code) from EezEntity where upper(name) like :name OR upper(code) like :code AND enabled='Y' GROUP BY gid)")
+        @NamedQuery(name = EezEntity.SEARCH_EEZ_NAMES_BY_CODE, query = "From EezEntity where code in (SELECT distinct(code) from EezEntity where (upper(name) like :name OR upper(code) like :code) AND enabled='Y' GROUP BY gid)")
 })
 @Table(name = "eez")
 @EqualsAndHashCode(callSuper = true)
