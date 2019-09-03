cc9e0a0fd0c32d63c63fe1a56b6f10ea421e6cd1
Kasim Gul <kasimgul@gmail.com>
2019-09-03 10:09:08 +0200

    Adding cors support

--- rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/util/RequestFilter.java
+++ rest/src/main/java/eu/europa/ec/fisheries/uvms/spatial/rest/util/RequestFilter.java
@@ -9,25 +9,41 @@ details. You should have received a copy of the GNU General Public License along
 
  */
 
-
 package eu.europa.ec.fisheries.uvms.spatial.rest.util;
 
 import eu.europa.ec.fisheries.uvms.spatial.rest.constants.RestConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Resource;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.ForbiddenException;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebFilter("/*")
 public class RequestFilter implements Filter {
 
-    public static final int DEFAULT_SRID = 4326;
+    private static final Logger LOG = LoggerFactory.getLogger(RequestFilter.class);
 
-
-    final static Logger LOG = LoggerFactory.getLogger(RequestFilter.class);
+    /**
+     * {@code corsOriginRegex} is valid for given host names/IPs and any range of sub domains.
+     *
+     * localhost:28080
+     * localhost:8080
+     * 127.0.0.1:28080
+     * 127.0.0.1:8080
+     * 192.168.***.***:28080
+     * 192.168.***.***:8080
+     * *.hav.havochvatten.se:8080
+     * *.hav.havochvatten.se:28080
+     */
+    @Resource(lookup = "java:global/cors_allowed_host_regex")
+    private String corsOriginRegex;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -36,11 +52,26 @@ public class RequestFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        final String HOST = httpServletRequest.getHeader("HOST");
+
+        boolean isValid = validateHost(HOST);
+
+        if (!isValid)
+            throw new ForbiddenException("You are not allowed to make any request from an external domain. Your Host: " + HOST);
+
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_ORIGIN, RestConstants.ACCESS_CONTROL_ALLOW_METHODS_ALL);
         response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_METHODS, RestConstants.ACCESS_CONTROL_ALLOWED_METHODS);
         response.setHeader(RestConstants.ACCESS_CONTROL_ALLOW_HEADERS, RestConstants.ACCESS_CONTROL_ALLOW_HEADERS_ALL);
         chain.doFilter(request, res);
+    }
+
+    private boolean validateHost(String host) {
+        Pattern pattern = Pattern.compile(corsOriginRegex);
+        Matcher matcher = pattern.matcher(host);
+        return matcher.matches();
     }
 
     @Override
