1b7b1f7770722d99a8c132221f88e3c990c01039
Nikos Paraskevopoulos <Nikolaos.PARASKEVOPOULOS@sword-group.com>
2020-03-03 17:51:56 +0200

    [UNIONVMS-4431] Fixed Arquillian configuration/retrieve the application URL using test.properties
    Also some clean-up, e.g. the useless "logfile_IS_UNDEFINED" files.

--- rest/src/test/java/eu/europe/ec/fisheries/uvms/spatial/rest/BuildSpatialRestDeployment.java
+++ rest/src/test/java/eu/europe/ec/fisheries/uvms/spatial/rest/BuildSpatialRestDeployment.java
@@ -1,9 +1,14 @@
 package eu.europe.ec.fisheries.uvms.spatial.rest;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.UriBuilder;
 
 import eu.europa.ec.fisheries.uvms.commons.rest.filter.MDCFilter;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
@@ -18,12 +23,16 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 @ArquillianSuiteDeployment
 public abstract class BuildSpatialRestDeployment {
 
+    private static final String PROPERTIES_FILE = "test.properties";
+    private static final String DEFAULT_WF_HOST = "localhost";
+    private static final String DEFAULT_WF_PORT = "8080";
+
     @Deployment(name = "spatial", order = 1)
     public static Archive<?> createDeployment() {
 
         WebArchive testWar = ShrinkWrap.create(WebArchive.class, "test.war");
 
-        File[] files = Maven.configureResolver().loadPomFromFile("pom.xml")
+        File[] files = Maven.configureResolver().workOffline().loadPomFromFile("pom.xml")
                 .resolve("eu.europa.ec.fisheries.uvms.spatial:service",
                         "eu.europa.ec.fisheries.uvms.movement:movement-model")
                 .withTransitivity().asFile();
@@ -40,12 +49,26 @@ public abstract class BuildSpatialRestDeployment {
         return testWar;
     }
 
-    protected WebTarget getWebTarget() {
+    private String makebaseUri() {
+        Properties properties = new Properties();
+        InputStream is = getClass().getResourceAsStream("/"+PROPERTIES_FILE);
+        if (is != null) {
+            try {
+                properties.load(is);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Class-path resource: '{}' does not exist. Using default values" + PROPERTIES_FILE);
+        }
 
+        return "http://" + properties.getProperty("wildfly_host", DEFAULT_WF_HOST) + ":" + properties.getProperty("wildfly_port", DEFAULT_WF_PORT);
+    }
+
+    protected WebTarget getWebTarget() {
         ObjectMapper objectMapper = new ObjectMapper();
         Client client = ClientBuilder.newClient();
         client.register(new JacksonJaxbJsonProvider(objectMapper, JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS));
-        return client.target("http://localhost:28080/test/spatialnonsecure");
-        //return client.target("http://localhost:8080/test/rest");
+        return client.target(UriBuilder.fromUri(makebaseUri()).path("test").path("spatialnonsecure").build());
     }
 }
