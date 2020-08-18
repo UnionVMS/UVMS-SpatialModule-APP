db11961d346b14486800d5048d45302a1c137401
lsotiriadis <leontion.sotiriadis@sword-group.com>
2020-08-18 14:03:35 +0300

    [UNIONVMS-4752] Added allowance in user config to add replacement attribute title

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/spatial/service/dto/usm/TitleDto.java
@@ -0,0 +1,51 @@
+/*
+Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries @ European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it 
+and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of 
+the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more 
+details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+
+ */
+package eu.europa.ec.fisheries.uvms.spatial.service.dto.usm;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TitleDto {
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("title")
+    private String title;
+
+    public TitleDto() {}
+
+    public TitleDto(String code,String title) {
+        this.code = code;this.title = title;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [code = " + code + ", title = "+title+"]";
+    }
+}
